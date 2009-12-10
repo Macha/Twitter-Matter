6fa3b545f5e0dd769c8a0ba0ad87225d7997b19d
Macha <macha.hack@gmail.com>
2009-12-10 20:31:43 +0000

    Initial commit

---
+++ tweet.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+
+import json
+import sys
+import urllib
+import urllib2
+
+class CLITweet:
+	
+	username = 'USERNAME'
+	password = 'PASSWORD'
+
+
+	def run(self):
+		try:
+			func = getattr(self, sys.argv[1])
+		# Command not found
+		except AttributeError:
+			print "The command", sys.argv[1], "doesn't exist"
+		# No action defined, show help
+		except IndexError:
+			self.help()
+		else:
+			if callable(func):
+				try:
+					func()
+				except IndexError:
+					print "Missing argument for command '" + sys.argv[1] + "' check help"
+
+	def setup(self):
+		# Prepares API call that needs authentication
+		password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
+		password_manager.add_password(
+			None, 'https://twitter.com/', self.username, self.password
+		)
+		auth_handler = urllib2.HTTPBasicAuthHandler(password_manager)
+		opener = urllib2.build_opener(auth_handler)
+		urllib2.install_opener(opener)
+				
+
+	def tweet(self):
+		# Tweet to API
+		self.setup()
+		tweet = ' '.join(sys.argv[2:])
+		if len(tweet) > 140:
+			print "Tweet is too long. Please limit yourself to 140 characters"
+			sys.exit(1)
+
+		params = urllib.urlencode({
+			'status': tweet
+		})
+		try:
+			data = urllib2.urlopen('http://twitter.com/statuses/update.json', params).read()
+			print data
+		except urllib2.HTTPError, e:
+			print e.code
+			print e.read()
+
+
+	def friends(self):
+		# Get latest tweets from friends
+		self.setup()
+		if len(sys.argv) == 3:
+			number_of_tweets = str(sys.argv[2])
+		else:
+			number_of_tweets = str(10)
+
+		try:
+			data = urllib2.urlopen('http://twitter.com/statuses/friends_timeline.json?count=' \
+			+ number_of_tweets).read()
+
+		except urllib2.HTTPError, e:
+			print e.code
+			print e.read()
+			sys.exit(1)
+
+		data = json.loads(data)
+		
+		for tweet in data:
+			print tweet['user']['screen_name'], ': \t', tweet['text']
+			print
+
+			
+		
+
+	def help(self):
+		# Print help information
+		print
+		print "Usage:"
+		print "\t tweet <tweet> - Sends a tweet"
+		print "\t friends [<num>] - Gets latest <num> tweets from friends"
+		print "\t help - Prints help information"
+		print
+
+CLITweet().run()
